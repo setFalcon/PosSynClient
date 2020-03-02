45aadb4b69c0d103e48f44b0d508ae171a581f11
setFalcon <1449530193@qq.com>
2020-03-02 14:57:33 +0800

    #003-添加PhotonEngine脚本并完善单例模式

---
+++ Assets/Scripts/PhotonEngine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class PhotonEngine : MonoBehaviour {
+    private static PhotonEngine Instance;
+
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != this) {
+            Destroy(gameObject);
+        }
+    }
+}
