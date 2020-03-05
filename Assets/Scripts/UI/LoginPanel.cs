979eca6c0652f2d4110ad6558e62dc31465c0653
setFalcon <1449530193@qq.com>
2020-03-05 17:17:50 +0800

    #012-完善界面之间跳转

---
+++ Assets/Scripts/UI/LoginPanel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginPanel : MonoBehaviour {
+    private Button loginButton;
+    private Button registerButton;
+    public GameObject registerPanel;
+
+    private void Awake() {
+        loginButton = GameObject.Find("Canvas/LoginPanel/LoginButton").GetComponent<Button>();
+        registerButton = GameObject.Find("Canvas/LoginPanel/RegisterButton").GetComponent<Button>();
+    }
+
+    private void Start() {
+        loginButton.onClick.AddListener(OnLoginButtonClick);
+        registerButton.onClick.AddListener(OnRegisterButtonClick);
+    }
+
+    private void OnLoginButtonClick() {
+        
+    }
+
+    private void OnRegisterButtonClick() {
+        registerPanel.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UI/RegisterPanel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RegisterPanel : MonoBehaviour {
+    private Button backButton;
+    private Button registerButton;
+    public GameObject loginPanel;
+
+    private void Awake() {
+        backButton = GameObject.Find("Canvas/RegisterPanel/BackButton").GetComponent<Button>();
+        registerButton = GameObject.Find("Canvas/RegisterPanel/RegisterButton").GetComponent<Button>();
+    }
+
+    private void Start() {
+        backButton.onClick.AddListener(OnBackButtonClick);
+        registerButton.onClick.AddListener(OnRegisterButtonClick);
+    }
+
+    private void OnBackButtonClick() {
+        loginPanel.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    private void OnRegisterButtonClick() { }
+}
