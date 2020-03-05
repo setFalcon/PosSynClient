0c09a6a92a7a477bee8ffde1fd79c8b8d6502e15
setFalcon <1449530193@qq.com>
2020-03-05 17:37:26 +0800

    #013-抽象请求类Request

---
+++ Assets/LoginRequest.cs
@@ -0,0 +1,9 @@
+﻿public class LoginRequest : Request {
+    public override void DefaultRequest() {//发起请求的代码
+        
+    }
+
+    public override void OnOperationResponse() {//响应的代码
+        
+    }
+}
---
+++ Assets/Scripts/Request/Request.cs
@@ -0,0 +1,8 @@
+﻿using ConnectBridge;
+using UnityEngine;
+
+public abstract class Request : MonoBehaviour {
+    private OperationCode OpCode; //操作码
+    public abstract void DefaultRequest();
+    public abstract void OnOperationResponse();
+}
