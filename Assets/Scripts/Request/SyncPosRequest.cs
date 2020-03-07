c9f69c457747e3cd9a00b6760ec6d305a796d681
setFalcon <1449530193@qq.com>
2020-03-07 15:11:59 +0800

    #020-添加位置同步请求发起代码

---
+++ Assets/Scripts/Request/SyncPosRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ConnectBridge;
+using ExitGames.Client.Photon;
+using UnityEngine;
+
+public class SyncPosRequest : Request {
+    [HideInInspector] public Vector3 position;
+
+    private void Awake() {
+        OpCode = OperationCode.SyncPos;
+    }
+
+    public override void DefaultRequest() {
+        Dictionary<byte, object> data = new Dictionary<byte, object>();
+        data.Add((byte) ParameterCode.Position, new Vector3Data {
+            x = position.x, y = position.y, z = position.z
+        });
+        PhotonEngine.Peer.OpCustom((byte) OperationCode.SyncPos, data, true);
+    }
+
+    public override void OnOperationResponse(OperationResponse resp) {
+        throw new NotImplementedException();
+    }
+}
