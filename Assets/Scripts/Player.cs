87628bcb7a923cbdf2bab4810c6d2c5661d5872e
setFalcon <1449530193@qq.com>
2020-03-07 17:43:08 +0800

    #024-添加同步其他玩家位置的请求

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     public bool isLocalPlayer = true;
+    [HideInInspector] public string username;
     private SyncPosRequest _syncPosRequest;
+    private SyncPlayerRequest _syncPlayerRequest;
     private Vector3 lastPosition = Vector3.zero;
     private float moveOffset = 0.1f;
 
     private void Start() {
         if (isLocalPlayer) {
-            GetComponent<Renderer>().sharedMaterial.color = Color.green;
+            GetComponent<Renderer>().material.color = Color.green;
             _syncPosRequest = GetComponent<SyncPosRequest>();
+            _syncPlayerRequest = GetComponent<SyncPlayerRequest>();
+            _syncPlayerRequest.DefaultRequest();
             InvokeRepeating(nameof(SyncPosition), 3f, 0.05f);
         }
     }
---
+++ Assets/Scripts/Request/SyncPlayerRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ConnectBridge;
+using ExitGames.Client.Photon;
+using UnityEngine;
+
+public class SyncPlayerRequest : Request
+{
+   private void Awake() {
+      OpCode = OperationCode.SyncPlayer;
+   }
+
+   public override void DefaultRequest() {
+      PhotonEngine.Peer.OpCustom((byte) OpCode, null, true);
+   }
+
+   public override void OnOperationResponse(OperationResponse resp) {
+   }
+}
