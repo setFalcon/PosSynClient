e877fe7b3dd47e46bac5fce0cdefa758f51ab462
setFalcon <1449530193@qq.com>
2020-03-07 18:15:11 +0800

    #025-反序列化Json,处理服务器用户列表响应结果

--- Assets/Scripts/Request/SyncPlayerRequest.cs
+++ Assets/Scripts/Request/SyncPlayerRequest.cs
@@ -2,19 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using ConnectBridge;
+using ConnectBridge.Util;
 using ExitGames.Client.Photon;
+using LitJson;
 using UnityEngine;
 
-public class SyncPlayerRequest : Request
-{
-   private void Awake() {
-      OpCode = OperationCode.SyncPlayer;
-   }
+public class SyncPlayerRequest : Request {
+    private void Awake() {
+        OpCode = OperationCode.SyncPlayer;
+    }
 
-   public override void DefaultRequest() {
-      PhotonEngine.Peer.OpCustom((byte) OpCode, null, true);
-   }
+    public override void DefaultRequest() {
+        PhotonEngine.Peer.OpCustom((byte) OpCode, null, true);
+    }
 
-   public override void OnOperationResponse(OperationResponse resp) {
-   }
+    public override void OnOperationResponse(OperationResponse resp) {
+        string usernameListJson = (string) DictUtil.GetValue(resp.Parameters, (byte) ParameterCode.UsernameList);
+        List<string> usernameList = JsonMapper.ToObject<List<string>>(usernameListJson);
+        foreach (string name in usernameList) {
+            Debug.Log(name);
+        }
+    }
 }
