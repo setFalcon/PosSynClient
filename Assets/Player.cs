99cf95637d001640e5c94a0c8508478894e95c84
setFalcon <1449530193@qq.com>
2020-03-07 14:59:42 +0800

    #019-控制主角移动

---
+++ Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public bool isLocalPlayer = true;
+
+    private void Start() {
+        if (isLocalPlayer) {
+            GetComponent<Renderer>().sharedMaterial.color = Color.green;
+        }
+    }
+
+    private void Update() {
+        if (isLocalPlayer) {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            transform.Translate(Time.deltaTime * 4f * new Vector3(h, 0, v));
+        }
+    }
+}
